25865455119754494fddc2e691962ef08092d2b0
abderrahim si ziani <abderrahim.siziani@gmail.com>
2017-02-03 23:14:54 +0100

    feat(angular) : first commit
    starting project

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,14 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name firstAppApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the firstAppApp
+ */
+angular.module('firstAppApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.helloWorld = "Hello, World!";
+    $scope.name = "Zéro";
+  });
